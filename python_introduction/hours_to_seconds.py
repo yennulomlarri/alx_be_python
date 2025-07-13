ab7dd647911b16128ce3ae46e707d77e7c07eb9d
Mateiyendou Kombat <mateiyendoukombat@gmail.com>
2025-07-13 01:36:01 +0000

    Completed Python Introduction project

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,8 @@
+# Define the number of hours
+hours = 2
+
+# Calculate the equivalent number of seconds
+seconds = hours * 3600
+
+# Print the result in the required format
+print(f"{hours} hour(s) is {seconds} seconds.")
---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,9 @@
+# Define the dimensions of the rectangle
+length = 10
+width = 5
+
+# Calculate the area
+area = length * width
+
+# Print the calculated area
+print(f"The area of the rectangle is: {area}")
